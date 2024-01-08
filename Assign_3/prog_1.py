51aa9552b9523c299e642e1e93c6493c85bb71a6
Lucefer256 <sm30122000@gmail.com>
2024-01-08 23:22:45 +0530

    prog 1 of ass3

--- Assign_3/prog_1.py
+++ Assign_3/prog_1.py
@@ -1,1 +1,11 @@
 n=int(input("Enter the number of elements:"))
+str_list=[]
+c=0
+for i in range(n):
+    sr=input("Enter the string: ")
+    str_list.append(sr)
+    if(len(sr)>=2 and (sr[0]==sr[-1])):
+        c +=1
+
+print("List :",str_list)
+print("Result number :",c)
