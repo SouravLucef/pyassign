9be82d336653d955a6218e5ea6581b4fd5262cf3
Lucefer256 <sm30122000@gmail.com>
2024-01-12 23:57:59 +0530

    prog3

--- Assign_3/prog_2.py
+++ Assign_3/prog_2.py
@@ -1,1 +1,8 @@
-lst=[(1,2),(2,3),(2,1)]
+tu_list=[]
+
+n=int(input("Enter the list length: "))
+for i in range(n):
+    mytuple=tuple(int(x) for x in (input("Enter space seperated numbers:").split()))
+    tu_list.append(mytuple)
+print(f"Sample list: {tu_list}")
+print(f"Expected result:{sorted(tu_list,key=lambda elem: elem[1])}")
---
+++ Assign_3/prog_3.py
@@ -0,0 +1,9 @@
+
+my_list=input("Enther the elements as space seperated").split()
+new_list=[]
+for i in my_list:
+    if(i not in new_list):
+        new_list.append(i)
+        print(f"No. of {i} in List is {my_list.count(i)}")
+print(my_list)
+print(new_list)
