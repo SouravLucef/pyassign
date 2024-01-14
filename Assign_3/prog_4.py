74200cba54f7f4eed22d7fe4b711bd0226f21106
Lucefer256 <sm30122000@gmail.com>
2024-01-14 19:55:51 +0530

    prog4

--- Assign_3/prog_4.py
+++ Assign_3/prog_4.py
@@ -1,8 +1,11 @@
 disc= {'1':['a','b'], '2':['c','d']}
 
-print(len(disc['1']))
-for i in disc['1']:
+# print(len(disc['1']))
+# for i in disc['1']:
     
-    for j in disc['2']:
-        print(i+j)
+#     for j in disc['2']:
+#         print(i+j)
 
+keys=disc.keys()
+print(keys)
+print(keys[0])
--- Assign_3/prog_5.py
+++ Assign_3/prog_5.py
@@ -1,8 +1,8 @@
 my_dict = {'A': 67, 'B': 23, 'C': 45, 'D': 56, 'E': 12, 'F': 69}
-# x=reversed(my_dict.items())
-# print(x)'
-for k,v in reversed(my_dict.items()):
-    print(k,v)
-
-x=sorted(my_dict.values())
-print(x)
+sorted_disc=sorted(my_dict.values(),reverse=True)
+sorted_disc=sorted_disc[:3]
+print(sorted_disc)
+for i in sorted_disc:
+    for j in my_dict.keys():
+        if my_dict[j]==i:
+            print(f"{j}:{my_dict[j]}")
