935fcb23c2c9cb5f0d48ec80859eb1e30bf3ab6a
Lucefer256 <sm30122000@gmail.com>
2024-01-20 00:09:45 +0530

    modified prog 6

--- Assign_3/prog_4.py
+++ Assign_3/prog_4.py
@@ -1,4 +1,4 @@
-disc= {'1':['a','b','c'], '2':['d','e','f']}
+disc= {'1':['a','b','c'], '2':['d','e','f'],'3':['g','h','i']}
 
 # print(len(disc['1']))
 # for i in disc['1']:
@@ -7,7 +7,7 @@ disc= {'1':['a','b','c'], '2':['d','e','f']}
 #         print(i+j)
 l=list(disc.values())
 print(l)
-for i in range(len(l[0])-1):
+for i in range(len(l)-1):
     for j in l[i]:
         for k in l[i+1]:
             print(j+k)
