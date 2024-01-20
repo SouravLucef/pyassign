9ccd6524c22cf41e15e09b1906fd23eb44728be5
Lucefer256 <sm30122000@gmail.com>
2024-01-21 00:00:02 +0530

    p

---
+++ Assign_3/prog_11.py
@@ -0,0 +1,6 @@
+def substring(U_string,Substr):
+    return U_string.find(Substr)
+
+U_string="today is a rainy day"
+Substr="day"
+print("Resylt :",substring(U_string,Substr))
--- Assign_3/prog_4.py
+++ Assign_3/prog_4.py
@@ -1,20 +1,9 @@
 disc= {'1':['a','b','c'], '2':['d','e','f'],'3':['g','h','i']}
 
-# print(len(disc['1']))
-# for i in disc['1']:
-    
-#     for j in disc['2']:
-#         print(i+j)
 l=list(disc.values())
 print(l)
 for i in range(len(l)-1):
     for j in l[i]:
         for k in l[i+1]:
             print(j+k)
-'''for i in range(len(disc.values())-1):
-    for key,value in disc.items():
-        print(value[i],':',value[i+1])
-        '''
-
-
 
